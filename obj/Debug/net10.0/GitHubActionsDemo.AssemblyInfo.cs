57426690631a2b234da11d443f04dd3e9a4963a5
Ramasubbu M <ramasubbu@appspos.com>
2026-08-03 09:53:50 +0530

    revert it warnerror

--- obj/Debug/net10.0/GitHubActionsDemo.AssemblyInfo.cs
+++ obj/Debug/net10.0/GitHubActionsDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitHubActionsDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb2b524971aac2ff2d56540b0fe7e6aeaf0783f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db8e5b6489ca3e551c968721e3fb00e2de09573a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitHubActionsDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitHubActionsDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
